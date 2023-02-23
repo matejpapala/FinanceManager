edad1f1733130c3ac23fca34ca822412b58fdd3d
unknown <matejpapala1234@gmail.com>
2023-02-23 20:42:51 +0100

    FirstCommit

---
+++ AccountModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagerLibrary
+{
+    public class AccountModel
+    {
+        public float Balance { get; set; }
+        public string TypeOfAccount { get; set; }
+        public string OwnerOfAccount { get; set; }
+
+    }
+}
---
+++ ProfileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagerLibrary
+{
+    public class ProfileModel
+    {
+        public List<Account> Accounts { get; set; } = new List<Account>();
+        public string ProfileName { get; set; }
+
+    }
+}
